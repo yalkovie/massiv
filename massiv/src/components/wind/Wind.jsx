30df0d5bec89833051ed779e10af21eb1c8b29b3
Ilya <yalkovie@mail.ru>
2025-12-31 19:50:52 +0300

    end

--- massiv/src/components/wind/Wind.jsx
+++ massiv/src/components/wind/Wind.jsx
@@ -1,6 +1,15 @@
 import './Wind.css'
 
 function Wind({pic, title, brand, type, mm, distance, cost, dscrp, discount}) {
+
+    function getPrice() {
+        if (discount === 0) {
+            return cost.toFixed(2)
+        } else {
+            return (cost - cost * (0.01 * discount)).toFixed(2)
+        }
+    }
+
     return(
         <>
         <div className='product'>
@@ -8,9 +17,14 @@ function Wind({pic, title, brand, type, mm, distance, cost, dscrp, discount}) {
             <h2>{title}</h2>
             <p className='info'>Производитель: <span>{brand}</span></p>
             <p className='info'>Тип: <span>{type}</span></p>
-            <p className='info'>Апертура: <span>{mm}</span>мм</p>
-            <p className='info'>Фокусное расстояние: <span>{distance}</span>мм<span className='saleSize'>{discount}</span></p>
-            <p className='price'>Цена: <span>{cost}</span>$</p>
+            <p className='info'>Апертура: <span>{distance}</span>мм</p>
+            <p className='info'>Фокусное расстояние: <span>{mm}</span>мм</p>
+            <p className='price'>
+                Цена: <span>{getPrice()}</span>$
+                {
+                discount === 0 ? null : <span className='saleSize'>{"-" + discount + "%"}</span>
+                }
+                </p>
             <p className='description'>{dscrp}</p>
         </div>
         </>
