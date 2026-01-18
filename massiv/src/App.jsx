fe1fba88c26b60a136a90704efc87d3942c4cc41
Ilya <yalkovie@mail.ru>
2026-01-18 13:03:50 +0300

    second commit

--- massiv/src/App.jsx
+++ massiv/src/App.jsx
@@ -6,6 +6,12 @@ import Wind from './components/wind/Wind.jsx'
 function App() {
 
   let [bible, setBible] = useState([])
+  let filter = useRef(
+    {
+      "increase" : true,
+      "dis" : false
+  }
+)
 
   useEffect(() => {
     fetch('./products.json')
@@ -16,20 +22,54 @@ function App() {
     .catch(error => console.error('Ошибка загрузки:', error))
   }, [])
 
+  function change_dis(event) {
+    filter.current["dis"] = event.target.checked
+    changer()
+  }
+
+  function change_increase(el) {
+    filter.current["increase"] = el.target.value
+    changer()
+  }
+
+  // function changer() {
+  //   if (filter["increase"] === true) {
+
+  //   } else {
+
+  //   }
+
+  //   if (filter['dis'] === true) {
+
+  //   } else {
+      
+  //   }
+  // }
+
   return (
     <>
-      { bible.length > 0 ? bible.map(el => (<Wind 
-        key={el.id}
-        pic={el.image}
-        title={el.name}
-        brand={el.brand}
-        type={el.type}
-        mm={el.focalLength}
-        distance={el.lensDiameter}
-        cost={el.price}
-        dscrp={el.description}
-        discount={el.discountPercentage}
-      />)) : <p className='loading-text'>Загрузка данных...</p>}
+   <header>
+    <select name="filter" onChange={change_increase}>
+      <option value="increase">Цена по возростанию</option>
+      <option value="decrease">Цена по убыванию</option>
+    </select>
+      <input type="checkbox" id="sale-only" onChange={change_dis}/>
+      <label htmlFor="sale-only">только со скидкой</label>
+    </header>
+      <>
+        { bible.length > 0 ? bible.map(el => (<Wind 
+          key={el.id}
+          pic={el.image}
+          title={el.name}
+          brand={el.brand}
+          type={el.type}
+          mm={el.focalLength}
+          distance={el.lensDiameter}
+          cost={el.price}
+          dscrp={el.description}
+          discount={el.discountPercentage}
+        />)) : <p className='loading-text'>Загрузка...</p>}
+      </>
     </>
   )
 }
